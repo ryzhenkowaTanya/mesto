41cdb50f365c56316044b0594e6c2b4e4cb512ff
Ryzhenkowa Tatyana <ryzhenkowa.tatyana@yandex.ru>
2021-03-19 22:38:36 +0300

    Исправление ошибок итерация 1

--- scripts/script.js
+++ scripts/script.js
@@ -1,32 +1,32 @@
-let openEditFormButton = document.querySelector(".profile__button-edit");
-let closeEditFormButton = document.querySelector(".popup__button-close");
-let popupEdit = document.querySelector(".popup");
-let formElement = document.querySelector(".form");
-let inputName = document.querySelector(".form__input_name");
-let inputJob = document.querySelector(".form__input_job");
-let profileName = document.querySelector(".profile__name");
-let profileJob = document.querySelector(".profile__job");
+let openEditFormButton = document.querySelector('.profile__button-edit');
+let closeEditFormButton = document.querySelector('.popup__button-close');
+let popupEdit = document.querySelector('.popup');
+let formElement = document.querySelector('.popup__form');
+let inputName = document.querySelector('.popup__input_type_name');
+let inputJob = document.querySelector('.popup__input_type_job');
+let profileName = document.querySelector('.profile__name');
+let profileJob = document.querySelector('.profile__job');
 
-document.querySelector(".form__button-save");
-function openPopap() {
-    popupEdit.classList.add('popup_opened')
+function openPopup() {
+    inputName.value = profileName.textContent;
+    inputJob.value = profileJob.textContent;
+    popupEdit.classList.add('popup_opened');
 }
 
-function popupClose() {
-    popupEdit.classList.remove('popup_opened')
+function closePopup() {
+    popupEdit.classList.remove('popup_opened');
 }
 
-openEditFormButton.addEventListener("click", openPopap);
-closeEditFormButton.addEventListener("click", popupClose);
-
-function HandlerProfileSubmit(evt) {
+function handlerProfileSubmit(evt) {
     evt.preventDefault();
     profileName.textContent = inputName.value;
     profileJob.textContent = inputJob.value;
-    popupClose()
+    closePopup();
 }
 
-formElement.addEventListener("submit", HandlerProfileSubmit);
+openEditFormButton.addEventListener('click', openPopup);
+closeEditFormButton.addEventListener('click', closePopup);
+formElement.addEventListener('submit', handlerProfileSubmit);
 
 
 
