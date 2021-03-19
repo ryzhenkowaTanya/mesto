4c7d0ff9b38a81e768b5b9594d52fedb431dc5f8
Ryzhenkowa Tatyana <ryzhenkowa.tatyana@yandex.ru>
2021-03-19 17:20:18 +0300

    Исправление ошибок итерация 1

--- scripts/script.js
+++ scripts/script.js
@@ -1,32 +1,32 @@
-let profileIcon = document.querySelector(".profile__button-edit");
-let formClose = document.querySelector(".popup__button-close");
-let popupOpened = document.querySelector(".popup_opened");
+let openEditFormButton = document.querySelector(".profile__button-edit");
+let closeEditFormButton = document.querySelector(".popup__button-close");
+let popupEdit = document.querySelector(".popup");
+let formElement = document.querySelector(".form");
+let inputName = document.querySelector(".form__input_name");
+let inputJob = document.querySelector(".form__input_job");
+let profileName = document.querySelector(".profile__name");
+let profileJob = document.querySelector(".profile__job");
+let buttonSave = document.querySelector(".form__button-save");
 
-function editProfile() {
-    popupOpened.style.display = "flex";
+function openPopap() {
+    popupEdit.classList.add('popup_opened')
 }
 
 function popupClose() {
-    popupOpened.style.display = "none";
+    popupEdit.classList.remove('popup_opened')
 }
 
-profileIcon.addEventListener("click", editProfile);
-formClose.addEventListener("click", popupClose);
+openEditFormButton.addEventListener("click", openPopap);
+closeEditFormButton.addEventListener("click", popupClose);
 
-let formElement = document.querySelector(".form");
-let inputName = document.querySelector(".form__input-name");
-let inputJob = document.querySelector(".form__input-job");
-
-let profileName = document.querySelector(".profile__name");
-let profileJob = document.querySelector(".profile__job");
-
-function formSubmitHandler(evt) {
+function HandlerProfileSubmit(evt) {
     evt.preventDefault();
     profileName.textContent = inputName.value;
     profileJob.textContent = inputJob.value;
-    popupOpened.style.display = "none";
+    popupClose()
 }
 
-formElement.addEventListener("submit", formSubmitHandler);
-let buttonSave = document.querySelector(".form__button-save");
-buttonSave.addEventListener("click", formSubmitHandler);
+formElement.addEventListener("submit", HandlerProfileSubmit);
+
+
+
