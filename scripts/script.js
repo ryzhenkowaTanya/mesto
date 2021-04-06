df239f58b359f07d6ad767323906c816879d1d2d
Ryzhenkowa Tatyana <ryzhenkowa.tatyana@yandex.ru>
2021-04-07 02:16:08 +0300

    Реализация лайка, открытие попапа с картинкой

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 let openEditFormButton = document.querySelector('.profile__button-edit');
 let closeEditFormButton = document.querySelector('.popup__button-close');
-let closeEditCardButton = document.querySelector('.popup__button-close_card');
+let closeEditCardButton = document.querySelector('.popup-create-card-close');
 let popupEditProfile = document.querySelector('.popup');
 let formElement = document.querySelector('.popup__form');
 let inputName = document.querySelector('.popup__input_type_name');
@@ -40,27 +40,33 @@ formElement.addEventListener('submit', handlerProfileSubmit);
 const initialCards = [
     {
         name: 'Pulau Ubin',
-        link: 'https://images.unsplash.com/photo-1617015606776-c54fd56b69b8?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80'
+        link: 'https://images.unsplash.com/photo-1617015606776-c54fd56b69b8?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80',
+         alt: ''
     },
     {
         name: 'Portovenere',
-        link: 'https://images.unsplash.com/photo-1617102888614-ae5c7c90d7eb?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80'
+        link: 'https://images.unsplash.com/photo-1617102888614-ae5c7c90d7eb?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80',
+        alt: ''
     },
     {
         name: 'Takayama',
-        link: 'https://images.unsplash.com/photo-1616666720355-03ce7f70b237?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80'
+        link: 'https://images.unsplash.com/photo-1616666720355-03ce7f70b237?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80',
+        alt: ''
     },
     {
         name: 'Ortygia',
-        link: 'https://images.unsplash.com/photo-1612361814394-35785ba16dc3?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=702&q=80'
+        link: 'https://images.unsplash.com/photo-1612361814394-35785ba16dc3?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=702&q=80',
+        alt: ''
     },
     {
         name: 'Novara di Sicilia',
-        link: 'https://images.unsplash.com/photo-1612361808300-da9583e1b34e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=671&q=80'
+        link: 'https://images.unsplash.com/photo-1612361808300-da9583e1b34e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=671&q=80',
+        alt: ''
     },
     {
         name: 'Gíza',
-        link: 'https://images.unsplash.com/photo-1590133324192-1df305deea6b?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1252&q=80'
+        link: 'https://images.unsplash.com/photo-1590133324192-1df305deea6b?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1252&q=80',
+        alt: ''
     }
 ];
 
@@ -68,21 +74,21 @@ const initialCards = [
 const cardList = document.querySelector('.cards__list');
 initialCards.forEach(item => {
     addCartInList(item.name, item.link)
-})
+});
 
 // открытие модального окна создания карточки
 
-const popupCreateCard = document.querySelector('.popup_create-card')
+const popupCreateCard = document.querySelector('.popup_create-card');
 const buttonCreateCard = document.querySelector('.profile__button-add');
 
 
-buttonCreateCard.addEventListener('click', () => openPopup(popupCreateCard))
+buttonCreateCard.addEventListener('click', () => openPopup(popupCreateCard));
 
 // создание новой карточки
 
-const nameInputCard = document.querySelector('.popup__input_type_name-card')
-const linkInputCard = document.querySelector('.popup__input_type_link')
-const createCardBtn = document.querySelector('.popup__button_create-card')
+const nameInputCard = document.querySelector('.popup__input_type_name-card');
+const linkInputCard = document.querySelector('.popup__input_type_link');
+const createCardBtn = document.querySelector('.popup__button_create-card');
 
 function handlerCardSubmit(evt) {
     evt.preventDefault();
@@ -92,28 +98,35 @@ function handlerCardSubmit(evt) {
     closePopup(popupCreateCard)
 }
 
-
 function createCard(nameCard, linkCard) {
-    const templateCards = document.querySelector('#card-template').content.querySelector('.card')
+    const templateCards = document.querySelector('#card-template').content.querySelector('.card');
     const templateElement = templateCards.cloneNode(true);
-    const templateCardsTitle = templateElement.querySelector('.card__title')
-    const templateCardsImage = templateElement.querySelector('.card__image')
+    const templateCardsTitle = templateElement.querySelector('.card__title');
+    const templateCardImage = templateElement.querySelector('.card__image');
+    templateCardImage.alt = templateCardsTitle.textContent;
     templateCardsTitle.textContent = nameCard;
-    templateCardsImage.src = linkCard;
+    templateCardImage.src = linkCard;
 // реализация лайка
     templateElement.querySelector('.card__button-like').addEventListener('click', evt => {
         evt.target.classList.toggle('like-active')
-    })
+    });
 // удаление карточки
-    const deleteCard = templateElement.querySelector('.card__button-delete')
-    deleteCard.addEventListener('click', () => templateElement.remove())
+    const deleteCard = templateElement.querySelector('.card__button-delete');
+    deleteCard.addEventListener('click', () => templateElement.remove());
 // открытие картинки в отдельном окне
-    const popupImage = document.querySelector('.popup-image')
-    const cardImage = templateElement.querySelector('.card__image')
-    const popupOpenImage = document.querySelector('.popup-image__image')
-    popupOpenImage.src = cardImage.src
+    const popupImage = document.querySelector('.popup-image');
+    const cardImage = templateElement.querySelector('.card__image');
+    const popupOpenImage = document.querySelector('.popup-image__image');
+    const popupImageBtnClose = document.querySelector('.popup-image-close');
+    const popupImageSignature = document.querySelector('.popup-image__signature');
+    const popupCardTitle = templateElement.querySelector('.card__title');
     cardImage.addEventListener('click', () => {
+        popupOpenImage.src = cardImage.src
+        popupImageSignature.textContent = popupCardTitle.textContent;
         openPopup(popupImage)
+    });
+    popupImageBtnClose.addEventListener('click', () => {
+        closePopup(popupImage)
     })
 
     return templateElement
@@ -125,7 +138,7 @@ function addCartInList(nameCard, linkCard) {
     cardList.prepend(templateElement)
 }
 
-createCardBtn.addEventListener('click', handlerCardSubmit)
+createCardBtn.addEventListener('click', handlerCardSubmit);
 
 
 
